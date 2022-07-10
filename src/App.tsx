2d72acfcc15da44b2343130bdb78dcfdacec09a8
Hendrik Kao <seemus790@hotmail.com>
2022-07-10 16:41:58 +0200

    Add "Event list" screen
    * Add "React Router" package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Events } from "./screens/Events/Events";
 
 function App() {
-  return <h1>Colony event list</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Events />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/screens/Events/Events.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Events() {
+  return <h1>Colony event list</h1>;
+}
